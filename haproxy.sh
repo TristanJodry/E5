a5c7341d8e7dae0b01ecd8582b5e4056f0e030d1
TristanJodry <126975568+TristanJodry@users.noreply.github.com>
2023-12-18 18:38:42 +0100

    Add files via upload

---
+++ haproxy.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo -e "frontend proxypublic\    mode http\n    bind *:80\n    acl regle1 hdr(host) site1.tgplus.net\n    acl regle2 hdr(host) site2.tgplus.net\n\n    use_backend site1 if regle1\n    use_backend site2 if regle2\n\nbackend site1\n    mode http\n    option httpchk\n    server web1 192.168.100.4\nbackend site2\n    mode http\n    option httpchk\n    server web2 192.168.100.5
+systemctl restart haproxy
